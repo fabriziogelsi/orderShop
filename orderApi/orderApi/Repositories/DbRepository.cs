d2cfd95851264583d15d6001f06022b22534642f
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2021-09-30 17:43:56 -0300

    Added mongoDb dependencies and manager

---
+++ orderApi/orderApi/Repositories/DbRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace orderApi.Repositories
+{
+    public class DbRepository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public DbRepository()
+        {
+            client = new MongoClient("mongodb://127.0.0.1:27017");
+
+            db = client.GetDatabase("shopDbFabrizioGelsi");
+        }
+    }
+}
